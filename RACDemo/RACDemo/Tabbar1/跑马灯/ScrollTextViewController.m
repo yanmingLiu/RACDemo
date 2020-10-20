979ebca0026e67381a59019aff5938979aad2ce8
lym <lwb374402328@gmail.com>
2020-10-20 23:30:36 +0800

    add

--- RACDemo/RACDemo/Tabbar1/跑马灯/ScrollTextViewController.m
+++ RACDemo/RACDemo/Tabbar1/跑马灯/ScrollTextViewController.m
@@ -17,14 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    ScrollTextView *textView = [[ScrollTextView alloc] initWithFrame:CGRectZero text:@"缓缓飘落的枫叶像思念" tont:[UIFont boldSystemFontOfSize:24] color:[UIColor orangeColor]];
+
+    ScrollTextView *textView = [[ScrollTextView alloc] initWithFrame:CGRectZero text:@"缓缓飘落的枫叶像思念" tont:[UIFont boldSystemFontOfSize:24] color:[UIColor whiteColor]];
     textView.cornerRadius = 6;
-    textView.backgroundColor = [UIColor lightGrayColor];
-    textView.borderWidth  = 1;
-    textView.borderColor = [UIColor blueColor];
+    textView.borderWidth = 2;
+    textView.borderColor = [UIColor orangeColor];
+    textView.clipsToBounds = YES;
     [self.view addSubview:textView];
-    
+
     textView.translatesAutoresizingMaskIntoConstraints = NO;
     [[textView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor constant:80] setActive:YES];
     [[textView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor constant:-80] setActive:YES];
@@ -32,11 +32,24 @@
     [[textView.heightAnchor constraintEqualToConstant:30] setActive:YES];
 
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(13 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [textView updateText:@"木造的甲板一整遍是那金黄" font:[UIFont boldSystemFontOfSize:24] color:[UIColor redColor]];
+        [textView updateText:@"木造的甲板一整遍是那金黄" font:[UIFont boldSystemFontOfSize:24] color:[UIColor whiteColor]];
+    });
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // gradient
+        CAGradientLayer *gl = [CAGradientLayer layer];
+        gl.frame = textView.bounds;
+        gl.startPoint = CGPointMake(0, 0.5);
+        gl.endPoint = CGPointMake(1, 0.5);
+
+        gl.colors = @[(__bridge id)[UIColor colorWithHexString:@"#7D6FFF" alpha:1].CGColor,
+                      (__bridge id)[UIColor colorWithHexString:@"#F05F2E" alpha:1].CGColor];
+        gl.locations = @[@(0), @(1.0f)];
+
+
+        [textView.layer insertSublayer:gl atIndex:0];
     });
 }
-
-
 
 /*
 #pragma mark - Navigation
